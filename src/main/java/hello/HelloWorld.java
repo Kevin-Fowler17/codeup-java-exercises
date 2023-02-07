1437b8b58ae0b87f8c3349ab12981f514ad903aa
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-02-07 11:36:18 -0600

    first exercise v1

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,9 +1,19 @@
 package hello;
 
+import java.sql.SQLOutput;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // the program starts executing here
         System.out.println("Hello world");
+        System.out.print("Hello world\n");
+
+        int myFavoriteNumber = 72;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string value";
+        System.out.println(myString);
     }
 }
