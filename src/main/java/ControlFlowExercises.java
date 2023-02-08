dd27a14e44db6e650768ef1c0a81f3fa26aa876b
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-02-08 16:43:48 -0600

    control statements exercise done

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -15,9 +15,10 @@ public class ControlFlowExercises {
 //            System.out.print(i + " ");
 //            i++;
 //        }
-//        for (int i = 5; i <= 15; i++) {
-//            System.out.print(i + " ");
-//        }
+        for (int i = 5; i <= 15; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
 
 
 //        int CountByTwo = 0;
@@ -25,9 +26,9 @@ public class ControlFlowExercises {
 //            System.out.println(CountByTwo);
 //            CountByTwo += 2;
 //        }while(CountByTwo <= 100);
-//        for (int CountByTwo = 0; CountByTwo <= 100; CountByTwo += 2) {
-//            System.out.println(CountByTwo);
-//        }
+        for (int CountByTwo = 0; CountByTwo <= 100; CountByTwo += 2) {
+            System.out.println(CountByTwo);
+        }
 
 
 //        int CountByFive = 100;
@@ -35,9 +36,9 @@ public class ControlFlowExercises {
 //            System.out.println(CountByFive);
 //            CountByFive -= 5;
 //        }while(CountByFive >= -10);
-//        for (int CountByFive = 100; CountByFive >= -10; CountByFive -= 5) {
-//            System.out.println(CountByFive);
-//        }
+        for (int CountByFive = 100; CountByFive >= -10; CountByFive -= 5) {
+            System.out.println(CountByFive);
+        }
 
 
 //        long Squared = 2;
@@ -45,48 +46,49 @@ public class ControlFlowExercises {
 //            System.out.println(Squared);
 //            Squared = Squared * Squared;
 //        }while(Squared < 1000000);
-//        for (long Squared = 2; Squared < 1000000; Squared *= Squared) {
-//            System.out.println(Squared);
-//        }
+        for (long Squared = 2; Squared < 1000000; Squared *= Squared) {
+            System.out.println(Squared);
+        }
 
 
-//        for (int i = 1; i <= 100 ; i++) {
-//            if (i % 3 == 0 && i % 5 != 0) {
-//                System.out.println("Fizz");
-//            } else if (i % 5 == 0 && i % 3 != 0) {
-//                System.out.println("Buzz");
-//            } else if (i % 3 == 0 && i % 5 == 0) {
-//                System.out.println("FizzBuzz");
-//            } else {
-//                System.out.println(i);
-//            }
-//        }
+        for (int i = 1; i <= 100 ; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0 && i % 3 != 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(i);
+            }
+        }
 
 
-//        String done = "yes";
-//        do {
-//
-//            System.out.print("What number would you like to go up to? ");
-//            int num = input.nextInt();
-//            double squared = 1;
-//            double cubed = 1;
-//
-//            System.out.printf("%nHere is your table!%n%n");
-//            System.out.printf("number | squared | cubed%n");
-//            System.out.printf("------ | ------- | -----%n");
-//
-//            for (int i = 1; i <= num; i++) {
-//                squared = Math.pow(i, 2);
-//                cubed = Math.pow(i, 3);
-//                System.out.printf("%-6d | %-7.0f | %-5.0f%n", i, squared, cubed);
-//            }
-//
-//            System.out.printf("%nDo you want to enter another number? ");
-//            done = input.next();
-//
-//        } while (done.equals("yes"));
+        String done = "yes";
+        do {
 
-        String done = "y";
+            System.out.print("What number would you like to go up to? ");
+            int num = input.nextInt();
+            double squared = 1;
+            double cubed = 1;
+
+            System.out.printf("%nHere is your table!%n%n");
+            System.out.printf("number | squared | cubed%n");
+            System.out.printf("------ | ------- | -----%n");
+
+            for (int i = 1; i <= num; i++) {
+                squared = Math.pow(i, 2);
+                cubed = Math.pow(i, 3);
+                System.out.printf("%-6d | %-7.0f | %-5.0f%n", i, squared, cubed);
+            }
+
+            System.out.printf("%nDo you want to enter another number? ");
+            done = input.next();
+
+        } while (done.equals("yes"));
+
+
+        done = "y";
         int grade = -1;
         do {
             do {
