539ec38b33992b24edb5b744627889201b77f4fe
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-02-09 10:47:48 -0600

    strings exercise

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,16 +1,35 @@
+import java.util.Scanner;
+
 public class Bob {
 
     public static void main(String[] args) {
 
-        // Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
-        // He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
-        // He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
-        // He answers 'Whatever.' to anything else.
+        Scanner input = new Scanner(System.in);
+
+        String question = "";
 
         do {
 
-        } while
+            System.out.print("Q: ");
+            question = input.nextLine();
 
+            if (question.endsWith("?")) {
+                // Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+                System.out.println("Sure.");
+            } else if (question.endsWith("!")) {
+                // He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+                System.out.println("Whoa, chill out!");
+            } else if (question.isEmpty()) {
+                // He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+                System.out.println("Fine. Be that way!");
+            } else {
+                // He answers 'Whatever.' to anything else.
+                System.out.println("Whatever.");
+            }
+
+        } while (!question.equalsIgnoreCase("Go to your room"));
+
+        input.close();
 
     }
 
