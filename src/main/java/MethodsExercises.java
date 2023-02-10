379cc18ea36c267273d76b22d9645916dca6bb31
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-02-10 14:05:16 -0600

    method exercises

--- src/main/java/MethodsExercises.java
+++ src/main/java/MethodsExercises.java
@@ -1,2 +1,125 @@
-package PACKAGE_NAME;public class MethodsExercises {
+import java.util.Scanner;
+import java.lang.Math;
+
+public class MethodsExercises {
+
+    public static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int multiplicationWithoutMultiplication (int num1, int num2) {
+        int total = 0;
+        for (int i = 1; i <= num2 ; i++) {
+            total += num1;
+        }
+        return total;
+    }
+
+    public static int multiplicationWithRecursion (int num1, int num2) {
+        if (num2 == 0) {
+            return 0;
+        }
+        if (num2 == 1) {
+            return num1;
+        }
+        return num1 + multiplicationWithRecursion(num1, num2 - 1);
+    }
+
+    public static double division(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        } else {
+            return num1 / num2;
+        }
+    }
+
+    public static double modulus(double num1, double num2) {
+        return num1 % num2;
+    }
+
+    public static int getIntger(Scanner input) {
+        System.out.print("Enter a number from 1 to 10: ");
+        int aNumber = input.nextInt();
+        if(aNumber < 1 || aNumber > 10) {
+            return getIntger(input);
+        }
+        return aNumber;
+    }
+
+    public static long factorial(int integer) {
+        long total = 1;
+        for (int i = 1; i <= integer; i++) {
+            total *= i;
+        }
+        return total;
+    }
+
+    public static int rollTheDice(int sides) {
+        return (int) (Math.random() * sides) + 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(MethodsExercises.addition(4, 5));
+
+        System.out.println(MethodsExercises.subtraction(4, 5));
+
+        System.out.println(MethodsExercises.multiplication(4, 5));
+        System.out.println(MethodsExercises.multiplicationWithoutMultiplication(9,9));
+        System.out.println(MethodsExercises.multiplicationWithRecursion(9,9));
+
+        System.out.println(MethodsExercises.division(20, 7));
+
+        System.out.println(MethodsExercises.modulus(9, 40));
+
+        Scanner input = new Scanner(System.in);
+        int n = getIntger(input);
+
+        String str = "";
+        int integer = 0;
+        do {
+            System.out.print("Enter an integer from 1 to 10: ");
+            integer = input.nextInt();
+            if (integer < 1 || integer > 10) {
+                System.out.println("The integer " + integer + " is out of range.");
+            } else {
+                System.out.println(MethodsExercises.factorial(integer));
+            }
+            System.out.print("Do you want to continue? ");
+            str = input.next();
+        } while (str.equalsIgnoreCase("y"));
+
+        str = "";
+        int sides = 0;
+        do {
+            System.out.print("How many sides do you want on the dice? ");
+            sides = input.nextInt();
+            System.out.print("Roll the dice when your are ready? ");
+            str = input.next();
+
+            if (str.equalsIgnoreCase("roll")) {
+                for (int i = 1; i <= 2; i++) {
+                    System.out.println(MethodsExercises.rollTheDice(sides));
+                }
+            }
+
+            System.out.print("Do you want to continue? ");
+            str = input.next();
+        } while (str.equalsIgnoreCase("y"));
+
+
+
+        input.close();
+
+    }
+
 }
--- src/main/java/MethodsLecture.java
+++ src/main/java/MethodsLecture.java
@@ -1,2 +1,74 @@
-package PACKAGE_NAME;public class MethodsLecture {
+import java.util.Scanner;
+
+public class MethodsLecture {
+    private static String myName;
+
+    // defining the function, and method overloading
+    public static String sayHello(String name) {
+        return String.format("Hello, %s!", name);
+    }
+    public static String sayHello() {
+        return String.format("Hello there!");
+    }
+    public static String sayHello(int numRepeats) {
+        String str = "";
+        for (int i = 0; i < numRepeats; i++) {
+            str += sayHello() + "!!\n";
+        }
+        return str;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        // since we are in this class, we don't need to have the class name
+        System.out.println(sayHello("bob"));
+        System.out.println(MethodsLecture.sayHello("bob"));
+
+        System.out.println(MethodsLecture.sayHello());
+
+        System.out.println(MethodsLecture.sayHello(5));
+
+
+//        myName = "jimbob";
+//        changeMyName(myName, "bob");
+//        System.out.println(myName);
+//
+//        myName = makeNewName("bob");
+
+
+
+        // Recursion
+        Scanner input = new Scanner(System.in);
+        int n = getNumberFromOneTo10(input);
+
+        input.close();
+
+
+
+    }
+
+    private static int getNumberFromOneTo10(Scanner input) {
+        System.out.print("Enter a number from 1 to 10: ");
+        int aNumber = input.nextInt();
+        if(aNumber < 1 || aNumber > 10) {
+            return getNumberFromOneTo10(input);
+        }
+        return aNumber;
+    }
+
+//    private static String makeNewName(String oldName, String newName) {
+//        myName = newName;
+//        System.out.println(myName);
+//    }
+
+//    private static void changeMyName(String oldName, String newName) {
+//
+//        oldName = newName;
+//        System.out.println("oldName is now " + oldName);
+//
+//    }
+
 }
