657fa6f111541a95a359dc3bbed0d88957d5521f
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-02-15 16:29:09 -0600

    rectangle and square exercise

--- src/main/java/shapes/Rectangle.java
+++ src/main/java/shapes/Rectangle.java
@@ -5,16 +5,18 @@ public class Rectangle {
     protected double length;
     protected double width;
 
-    public Rectangle(double l, double w) {
-        this.length = l;
-        this.width = w;
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
     }
 
     public double getArea() {
+        System.out.println("Rectangle get area.");
         return length * width;
     }
 
     public double getPerimeter () {
+        System.out.println("Rectangle get perimeter.");
         return (2 * length) + (2 * width);
     }
 
--- src/main/java/shapes/Square.java
+++ src/main/java/shapes/Square.java
@@ -8,16 +8,17 @@ public class Square extends Rectangle {
     public Square(double side) {
         super(side, side);
         this.side = side;
-
     }
 
     @Override
     public double getArea() {
+        System.out.println("Square get area.");
         return side * side;
     }
 
     @Override
     public double getPerimeter() {
+        System.out.println("Square get perimeter.");
         return 4 * side;
     }
 
