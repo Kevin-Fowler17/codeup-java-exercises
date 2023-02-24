8a3191e0b2c96958166ceea483be6d8af1cb4900
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-02-24 16:02:34 -0600

    done v1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
 
@@ -41,4 +44,14 @@ public class Test {
 //    public double getDouble() {
 //        return scanner.nextDouble();
 //    }
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(13);
+        numbers.add(new Integer(8));
+        numbers.add(3.6);
+    }
+
+
 }
