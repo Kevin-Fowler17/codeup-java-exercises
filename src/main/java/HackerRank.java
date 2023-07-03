f73f0fe8fc52a85f040c6fcce306f70b141c82bb
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-07-03 10:23:45 -0500

    hacker rank java problems

---
+++ src/main/java/HackerRank.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class HackerRank {
+
+    public static void main(String[] args) {
+
+//      1st challenge
+//        System.out.println("Hello, World.");
+//        System.out.println("Hello, Java.");
+
+//      2nd challenge
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        // Complete this line
+//        int b = scan.nextInt();
+//        // Complete this line
+//        int c = scan.nextInt();
+//
+//        System.out.println(a);
+//        // Complete this line
+//        System.out.println(b);
+//        // Complete this line
+//        System.out.println(c);
+
+    }
+}
