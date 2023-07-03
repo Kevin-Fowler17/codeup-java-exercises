178bc824f1e8929e42f47cf74c47dfb19be8521e
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-07-03 10:54:08 -0500

    hacker rank java problems third and fourth

--- src/main/java/HackerRank.java
+++ src/main/java/HackerRank.java
@@ -8,6 +8,7 @@ public class HackerRank {
 //        System.out.println("Hello, World.");
 //        System.out.println("Hello, Java.");
 
+
 //      2nd challenge
 //        Scanner scan = new Scanner(System.in);
 //        int a = scan.nextInt();
@@ -22,5 +23,30 @@ public class HackerRank {
 //        // Complete this line
 //        System.out.println(c);
 
+
+//        3rd challenge
+//        if (N % 2 != 0){
+//            System.out.println("Weird");
+//        } else if (N == 2 || N == 4) {
+//            System.out.println("Not Weird");
+//        } else if (N % 2 == 0 && (N >= 6 && N <= 20)) {
+//            System.out.println("Weird");
+//        } else {
+//            System.out.println("Not Weird");
+//        }
+
+
+//        4th challenge
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        // Write your code here.
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 }
