095d71cd18e259e5afe1c8fce5241bb8cba44291
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-07-03 14:35:27 -0500

    hacker rank java problems five and six

--- src/main/java/HackerRank.java
+++ src/main/java/HackerRank.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class HackerRank {
@@ -37,16 +40,49 @@ public class HackerRank {
 
 
 //        4th challenge
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
+//        Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//
+//        // Write your code here.
+//        double d = scan.nextDouble();
+//        scan.nextLine();
+//        String s = scan.nextLine();
+//
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
 
-        // Write your code here.
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
 
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+//        5th challenge
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("================================");
+//        for(int i=0;i<3;i++){
+//            String s1=sc.next();
+//            int x=sc.nextInt();
+//            //Complete this line
+//            System.out.printf("%-15s%03d%n", s1, x);
+//        }
+//        System.out.println("================================");
+
+
+//        6th challenge
+//        public class Solution {
+//            public static void main(String[] args) throws IOException {
+//                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//
+//                int N = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//                for (int i = 1; i < 11; i++) {
+//                    System.out.println(N + " x " + i + " = " + (N*i));
+//                }
+//
+//                bufferedReader.close();
+//            }
+//        }
+
+
+//        7th challenge
+
+
     }
 }
